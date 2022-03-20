8041fe57070f2b06a0419ed1794e116b79febe54
Andrei B <andreib@ysalinksystem.com>
2022-03-20 18:56:41 +0200

    Components

--- confusion-app/src/App.js
+++ confusion-app/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import './App.css';
+import {DISHES} from "./shared/dishes.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +23,7 @@ class App extends Component {
             </NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
