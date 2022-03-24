bed9660436637958ab7efc7fed41d7a2fbce4b27
working-web <working.web31@gmail.com>
2022-03-24 07:20:15 +0200

    coomit with errors

--- confusion-app/src/components/DishdetailComponent.js
+++ confusion-app/src/components/DishdetailComponent.js
@@ -1,52 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle, List } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {}
+function RenderDish({ dish }) {
+    if (dish !== null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody className="text-left">
+                        <CardTitle tag="h5">{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
     }
+}
 
-    renderDish(dish) {
-        if(dish !== undefined) {
-            return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name} />
-                            <CardBody className="text-left">
-                                <CardTitle tag="h5">{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div><div className="col-12 col-md-5 m-1">
-                        <h4 className="h4 text-left">Comments</h4>
-                        <List type="unstyled" className="text-left">
-                            <li>Imagine all the eatables, living in conFusion</li>
-                            <li className="mb-1">-- John Lemon, Oct 17, 2012</li>
-                            <li>Sends anyone heacven, I wish I could get my mother-in-law to eat it!</li>
-                            <li className="mb-1">-- Paul McVites, Sep 06, 2014</li>
-                            <li>Eat it, just eat it!</li>
-                            <li className="mb-1">-- Michael Jankishan, Feb 14, 2015</li>
-                            <li>ultimate, Reaching for the stars!</li>
-                            <li className="mb-1">--Ringo Starry, Dec 03, 2013</li>
-                            <li>It's your birdthday, we're gonna party!</li>
-                            <li className="mb-1">--25 Cent, Dec 03, 2011</li>
-                        </List>
-                    </div>
-                </div>
-            );
-        } else {
-            return <div></div>
-        }
-
+function RenderComments({ comments }) {
+    if (comments !== null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <List type="unstyled" className="text-left">
+                    {comments.comments.map((comment) => {
+                        return (
+                            <li key={comment.id} className="mb-1">
+                                <p>{comment.comment}</p>
+                                <p>--{ comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    }) }
+                </List>
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        );
     }
+}
 
-    render() {
-        return this.renderDish(this.props.dish);
-    }
-    
+const DishDetail = (props) => {
+    console.log(props)
+    return (
+        <div className="container">
+            <div className="row">
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish} />
+            </div>
+        </div>
+    );
 }
 
 export default DishDetail
--- confusion-app/src/shared/dishes.js
+++ confusion-app/src/shared/dishes.js
@@ -1,34 +1,80 @@
-export const DISHES = [
+export const DISHES = [{
+        id: 0,
+        name: 'Uthappizza',
+        image: 'assets/images/uthappizza.png',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comments: [{
+            id: 1,
+            author: 'John Lemon',
+            date: 'Oct 17, 2012',
+            comment: 'Imagine all the eatables, living in conFusion'
+        }, {
+            id: 2,
+            author: 'Paul McVites',
+            date: 'Sep 06, 2014',
+            comment: 'Sends anyone heacven, I wish I could get my mother-in-law to eat it!'
+        }, {
+            id: 3,
+            author: 'Michael Jankishan',
+            date: 'Feb 14, 2015',
+            comment: 'Eat it, just eat it!'
+        }, {
+            id: 4,
+            author: 'Ringo Starry',
+            date: 'Dec 03, 2013',
+            comment: 'ultimate, Reaching for the stars!'
+        }, {
+            id: 5,
+            author: '25 Cent',
+            date: 'Dec 03, 2011',
+            comment: 'It\'s your birdthday, we\'re gonna party!'
+        }]
+    },
+
     {
-      id: 0,
-      name:'Uthappizza',
-      image: 'assets/images/uthappizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-   {
-      id: 1,
-      name:'Zucchipakoda',
-      image: 'assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label:'',
-      price:'1.99',
-      description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-   {
-      id: 2,
-      name:'Vadonut',
-      image: 'assets/images/vadonut.png',
-      category: 'appetizer',
-      label:'New',
-      price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-   {
-      id: 3,
-      name:'ElaiCheese Cake',
-      image: 'assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label:'',
-      price:'2.99',
-      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
+        id: 1,
+        name: 'Zucchipakoda',
+        image: 'assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label: '',
+        price: '1.99',
+        description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        comments: [{
+            id: 1,
+            author: 'John Lemon',
+            date: 'Oct 17, 2012',
+            comment: 'Imagine all the eatables, living in conFusion'
+        }]
+    }, {
+        id: 2,
+        name: 'Vadonut',
+        image: 'assets/images/vadonut.png',
+        category: 'appetizer',
+        label: 'New',
+        price: '1.99',
+        description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        comments: [{
+            id: 1,
+            author: 'John Lemon',
+            date: 'Oct 17, 2012',
+            comment: 'Imagine all the eatables, living in conFusion'
+        }]
+    }, {
+        id: 3,
+        name: 'ElaiCheese Cake',
+        image: 'assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label: '',
+        price: '2.99',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        comments: [{
+            id: 1,
+            author: 'John Lemon',
+            date: 'Oct 17, 2012',
+            comment: 'Imagine all the eatables, living in conFusion'
+        }]
+    }
 ];
