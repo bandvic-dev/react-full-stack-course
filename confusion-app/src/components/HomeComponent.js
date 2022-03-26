b829e0d923b2c753b156f11bf75d81b16f9a511a
working-web <working.web31@gmail.com>
2022-03-26 15:32:30 +0200

    Single Page Applications Part 1

--- confusion-app/src/components/HomeComponent.js
+++ confusion-app/src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle><h4>{item.name}</h4></CardTitle>
+                {item.designation ? <CardSubtitle><h5>{item.designation}</h5></CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
     return (
         <div className="container">
-            <h1>Home</h1>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={ props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={ props.promotions} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={ props.leader} />
+                </div>
+            </div>
         </div>
     );
 }
--- confusion-app/src/components/MainComponent.js
+++ confusion-app/src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -29,7 +36,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotions={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={ this.state.leaders.filter((leader) => leader.featured)[0] }
+                />
             );
         }
 
@@ -38,12 +48,20 @@ class Main extends Component {
                 <Menu dishes={this.state.dishes}/>
             );
         }
+
+        const ContactPage = () => {
+            return (
+                <Contact />
+            );
+        }
+
         return (
             <div>
                 <Header />
                 <Routes>
                     <Route path="/home" element={HomePage()} />
                     <Route exact path="/menu" element={MenuPage()} />
+                    <Route exact path="/contactus" element={ContactPage()} />
                     <Route path="*" element={<Navigate to="/home" replace />}/>
                 </Routes>
                 <Footer />
