e0c52ac1b154961d2210f7ddecd297210cb434bd
Andrei B <andreib@usalinksystem.com>
2022-04-12 15:27:21 +0300

    React Animations

--- confusion-app/src/components/MainComponent.js
+++ confusion-app/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
     return {
@@ -86,14 +87,18 @@ class Main extends Component {
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route exact path="/aboutus" component={AboutPage} />
-                    <Route exact path="/menu" component={MenuPage} />
-                    <Route exact path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Redirect to="/" />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route exact path="/" component={HomePage} />
+                            <Route exact path="/aboutus" component={AboutPage} />
+                            <Route exact path="/menu" component={MenuPage} />
+                            <Route exact path="/menu/:dishId" component={DishWithId} />
+                            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
